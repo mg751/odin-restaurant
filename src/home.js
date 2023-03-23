ef67cbc0262a686bbf36a237856245060596c61d
mg751 <mgoreansky@gmail.com>
2023-03-23 21:28:16 +0200

    Add basic styling, nav skeleton

--- src/home.js
+++ src/home.js
@@ -3,15 +3,45 @@ export const generateHomepage = function () {
   let restaurantImg = document.createElement("img");
   let restaurantDesc = document.createElement("p");
 
-  let homeContent = [restaurantTitle, restaurantImg, restaurantDesc];
+  let x = [restaurantTitle, restaurantImg, restaurantDesc];
+
+  let container = document.createElement("div");
+  container.classList.add("home-content");
 
   restaurantTitle.innerText = "My stupid restaurant";
   restaurantImg.src = "http://unsplash.it/300/300?random&gravity=center";
   restaurantDesc.innerText =
     "Lorem ipsum dolor sist amet consectetur adipisicing elit. Estinventore assumenda in animi eum cupiditate incidunt, sequi aut libero";
 
-  for (let item of homeContent) {
-    content.appendChild(item);
+  content.appendChild(container);
+
+  for (let item of x) {
+    container.appendChild(item);
   }
-  return;
+};
+
+export const generateNav = () => {
+  let body = document.querySelector("body");
+
+  let container = document.createElement("div");
+  let home = document.createElement("p");
+  let menu = document.createElement("p");
+  let about = document.createElement("p");
+
+  home.innerText = "Home";
+  menu.innerText = "Menu";
+  about.innerText = "About";
+
+  container.classList.add("nav");
+  home.classList.add("home");
+  menu.classList.add("menu");
+  about.classList.add("about");
+
+  let nav = [home, menu, about];
+
+  for (let item of nav) {
+    container.appendChild(item);
+  }
+
+  body.insertBefore(container, content);
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { generateHomepage } from "./home";
 import "./style.css";
+import { generateNav } from "./home";
 
 let content = document.querySelector("#content");
 
 generateHomepage();
+generateNav();
